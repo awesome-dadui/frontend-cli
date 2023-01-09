9ed84e62debbaf61705682d14dc54e980792d0e5
bin.zeng <yy282371715@sina.cn>
2023-01-09 13:26:38 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,38 @@
 #!/usr/bin/env node
 
+'use strict'
+
 const program = require('commander')
-const package = require('../package.json')
+const config = require('../package.json')
 
 const {initCommand} = require('./cmd/init')
-const {addCommand} = require('./cmd/add')
 const {devCommand} = require('./cmd/dev')
+const {addCommand} = require('./cmd/add')
 
-program
-  .version(package.version)
-  .option('-d, --do', 'Do something', 'writing code')
-  .action(function (task) {
-    console.log(`Do this `, task)
-  })
-
-program
-  .command('add')
-  .description(`增加物料：-a增加api（dmall add -a），-c增加组件(damll add -c <name>)，-p增加页面（dmall add -p <args...>）`)
-  .option(`-a, --api`)
-  .option(`-c, --component <name>`)
-  .option(`-p, --page <args...>`)
-  .action((options) => {
-    addCommand(options)
-  })
+program.version(config.version)
 
 program
   .command('init')
-  .description('初始化项目模版')
-  .alias('i')
+  .description('初始化项目模版（pixi、react、vue）')
   .action(() => {
     initCommand()
   })
 
 program
   .command('dev')
+  .description('开发项目')
   .action(() => {
     devCommand()
+  })
+
+program
+  .command('add')
+  .description(`增加物料：-a增加api（fe add -a），-c增加组件(fe add -c <name>)，-p增加页面（fe add -p <args...>）`)
+  .option(`-a, --api`)
+  .option(`-c, --component <name>`)
+  .option(`-p, --page <args...>`)
+  .action((options) => {
+    addCommand(options)
   })
 
 program.parse(process.argv)
