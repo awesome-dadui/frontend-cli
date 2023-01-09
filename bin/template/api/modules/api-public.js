63e43304cd028d487117248c0f6d08faf1851af9
bin.zeng <yy282371715@sina.cn>
2023-01-09 13:29:27 +0800

    update

--- bin/template/api/modules/api-public.js
+++ bin/template/api/modules/api-public.js
@@ -1,33 +1,23 @@
-import {env, post, get, upload} from "../ajax";
+import {env, post, get, upload} from "../ajax"
 
-const HOST = `https://${env}magpie-partner.dmall.com`;
-let url = {
-  queryOffStoreStock: '/man/task/queryOffStoreStock', // 分页查询门店库存列表接口
-  uploadStore: '/man/task/lottery/uploadStore', // 上传门店库存列表
-  exportStore: '/man/task/lottery/exportStore', // 下载门店库存接口
-  removeStoreStock: '/man/task/removeStoreStock',//删除门店库存接口
-  saveStoreStock: '/man/task/saveStoreStock',//删除门店库存接口
-  countStoreStock: '/man/task/countStoreStock'
-}
-Object.keys(url).forEach(key => (url[key] = `${HOST}${url[key]}`))
+const HOST = `https://${env}.example.com`
 
 export let listApi = {
-  url,
-  getPath (name) {
+  getPath(name) {
     return HOST + `/toolkit/${name}/activityList`
   },
-  getActivityList (name, params) {
-    let url = HOST + `/toolkit/${name}/activityList`;
-    return get({url, params});
+  getActivityList(name, params) {
+    let url = HOST + `/toolkit/${name}/activityList`
+    return get({url, params})
   },
-  postSaveActivity (name, params) {
-    let url = HOST + `/toolkit/${name}/saveActivity`;
-    return post({url, params});
+  postSaveActivity(name, params) {
+    let url = HOST + `/toolkit/${name}/saveActivity`
+    return post({url, params})
   },
-  downloadExportStore (params) {
-    window.open(url.exportStore + '?rewardTempId=' + params.rewardTempId + '&rewardCfgId=' + params.rewardCfgId);
+  downloadExportStore(params) {
+    window.open(url.exportStore + '?rewardTempId=' + params.rewardTempId + '&rewardCfgId=' + params.rewardCfgId)
   },
-  uploadStoreFiles (params) {
+  uploadStoreFiles(params) {
     return upload({url: url.uploadStore, params})
   }
 }
